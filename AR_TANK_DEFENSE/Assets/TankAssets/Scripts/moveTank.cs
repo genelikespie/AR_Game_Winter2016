83293ac65e025739eca8680e0907a4bb3d7d53f8
gene shaffer <genenakamura@gmail.com>
2016-01-22 16:02:12 -0800

    Geney push
    bb

---
+++ AR_TANK_DEFENSE/Assets/TankAssets/Scripts/moveTank.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveTank : MonoBehaviour {
+
+    public GameObject Test;
+    Transform Spawner;
+    GameObject Tank;
+    Camera mainCamera;
+    Transform startTankLocation;
+    Transform endTankLocation;
+    float xScreen;
+    float yScreen;
+    float velocity;
+    float smoothTime;
+    Vector3 targetPosition;
+    Vector3 spawnerPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Tank = this.gameObject;
+        smoothTime = 5F;
+        velocity = 100F;
+        xScreen = Screen.width / 2;
+        yScreen = Screen.width / 2;
+        startTankLocation = Tank.transform;
+        Vector3 targetPosition = new Vector3(xScreen, yScreen, 0);
+        Spawner = GameObject.Find("spawnTankPad").transform;
+        spawnerPosition = Spawner.position;
+    }
+
+    //Frame Rate check for tank
+    void FixedUpdate()
+    {
+        /*
+        //  targetPosition = mainCamera.ScreenToWorldPoint(new Vector3(100, 100, mainCamera.nearClipPlane));
+        Vector3 fixedPosition = new Vector3(mainCamera.transform.position.x, spawnerPosition.y , mainCamera.transform.position.z);
+        targetPosition = fixedPosition + mainCamera.transform.rotation * Vector3.forward;
+        */
+
+
+        Vector3 moveA = startTankLocation.position;
+        RaycastHit Hit;
+        // if (Physics.Raycast(new Ray(mainCamera.transform.position, mainCamera.transform.rotation * Vector3.forward), out Hit))
+        Ray ray = mainCamera.ScreenPointToRay(new Vector3(xScreen, yScreen, 0));
+         if (Physics.Raycast(ray, out Hit))
+        {
+            Debug.DrawRay(ray.origin, ray.direction * 10000, Color.yellow);
+            // Debug.DrawLine(ray., Hit.transform.position, Color.red);
+            Tank.transform.position = Vector3.Slerp(moveA, Hit.point, velocity * Time.deltaTime / 100);
+            Debug.Log(Hit.transform.gameObject);
+            Debug.Log(Hit.point);
+            Debug.Log(moveA);
+        }
+
+
+
+    }
+        
+
+}
---
+++ AR_TANK_DEFENSE/Assets/TankAssets/Scripts/spawnTank.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnTank : MonoBehaviour {
+
+    GameObject tankSpawnPad;
+    public GameObject Tank;
+    Transform locationtankSpawnPad;
+    public bool spawnOnce;
+
+    IEnumerator hold(float timetohold)
+    {
+        yield return new WaitForSeconds(timetohold);
+        spawnerTank();
+    }
+
+
+	void Awake () {
+        tankSpawnPad = GameObject.Find("spawnTankZone");
+        locationtankSpawnPad = tankSpawnPad.transform;
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(hold(2f));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void spawnerTank()
+    {
+        Vector3 vectorTankPad = locationtankSpawnPad.position;
+        Instantiate(Tank,vectorTankPad,Tank.transform.rotation);
+        Debug.Log("TANK CREATED");
+    }
+}
