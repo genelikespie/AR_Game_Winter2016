511265b2109b9543f3210512e87e8cb243cec1ad
Jeffrey Shu <shusiming@gmail.com>
2016-02-08 20:45:18 -0800

    Back to main menu
    Button that, upon pressing, takes you back to the title screen.

---
+++ Assets/BackToMainScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BackToMainScript : MonoBehaviour {
+
+    public Button BTMButton;
+
+	// Use this for initialization
+	void Start () {
+        BTMButton = BTMButton.GetComponent<Button>();
+    }
+
+    public void backToMain()
+    {
+        Application.LoadLevel(2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
