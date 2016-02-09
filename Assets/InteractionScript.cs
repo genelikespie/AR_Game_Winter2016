9e7b2c9bd6ec7c531daf6726c6ebd54986709c38
Jeffrey Shu <shusiming@gmail.com>
2016-02-08 20:38:10 -0800

    Pause/Unpause
    Button text changes to "unpause" when paused. Pressing again will
    unpause and change text back to "pause."

--- Assets/InteractionScript.cs
+++ Assets/InteractionScript.cs
@@ -5,15 +5,27 @@ using System.Collections;
 public class InteractionScript : MonoBehaviour {
 
     public Button pauseButton;
+    bool paused;
 
 	// Use this for initialization
 	void Start () {
         pauseButton = pauseButton.GetComponent<Button>();
+        paused = false;
     }
 
     public void pauseGame()
     {
-        Time.timeScale = 0;
+        if (paused == false) {
+            Time.timeScale = 0;
+            paused = true;
+            pauseButton.GetComponent<Text>().text = "Unpause";
+        }
+        else
+        {
+            Time.timeScale = 1;
+            paused = false;
+            pauseButton.GetComponent<Text>().text = "Pause";
+        }
     }
 	
 	// Update is called once per frame
