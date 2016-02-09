f22a5dcf2518d2212f4bd45b8afb141b7353747b
Jeffrey Shu <shusiming@gmail.com>
2016-02-08 20:27:54 -0800

    Basic pause button
    It pauses things!

---
+++ Assets/InteractionScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InteractionScript : MonoBehaviour {
+
+    public Button pauseButton;
+
+	// Use this for initialization
+	void Start () {
+        pauseButton = pauseButton.GetComponent<Button>();
+    }
+
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
