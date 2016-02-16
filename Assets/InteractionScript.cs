e3e0d2f6edcecf0dea77086e1ef9543341e98836
Jeffrey Shu <shusiming@gmail.com>
2016-02-16 15:29:26 -0800

    Buttons 'fly' down when paused
    The buttons are now placed at a higher location above the camera. When
    the game is paused, then will translate down and stop at a certain
    point. Unpausing the game will cause them to return to their original
    position. Note: the y and z coordinates seem to be switched when it
    comes to the editor vs. the code.

--- Assets/InteractionScript.cs
+++ Assets/InteractionScript.cs
@@ -7,6 +7,9 @@ public class InteractionScript : MonoBehaviour {
     public Button pauseButton;
     public bool paused;
 
+    public GameObject pauseCylinder;
+    public GameObject BTMCylinder;
+
 	// Use this for initialization
 	void Start () {
         pauseButton = pauseButton.GetComponent<Button>();
@@ -25,11 +28,18 @@ public class InteractionScript : MonoBehaviour {
             Time.timeScale = 1;
             paused = false;
             pauseButton.GetComponent<Text>().text = "Pause";
+            pauseCylinder.transform.position = new Vector3(416, 1217, -107);
+            BTMCylinder.transform.position = new Vector3(416, 1217, -107);
         }
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        //Debug.Log(pauseCylinder.transform.position.x + " " + pauseCylinder.transform.position.y + " " + pauseCylinder.transform.position.z);
+        if (paused == true && pauseCylinder.transform.position.y > 200)
+        {
+            pauseCylinder.transform.Translate(Vector3.up * 30);
+            BTMCylinder.transform.Translate(Vector3.up * 30);
+        }
 	}
 }
