b12be1a2e3583a5e8c9b865d260d2c7720a539c0
Jeffrey Shu <shusiming@gmail.com>
2016-03-10 19:24:50 -0800

    Trying to implement tank selection
    Having a problem with the buttons right now, they don't seem to be
    interactive?

---
+++ Assets/Scripts/Tank/SelectTank.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SelectTank : MonoBehaviour {
+
+    public Button Tank1; 
+
+	// Use this for initialization
+	void Start () {
+        Tank1 = Tank1.GetComponent<Button>();
+    }
+
+    public void selectTank()
+    {
+        Debug.Log("selected!");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
